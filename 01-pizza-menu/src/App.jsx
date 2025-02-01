7d5373b1767632ab44c909ac1af8f2effba3cf0a
LiliGhaznavi <ghaznavizahra77@gmail.com>
2025-02-01 02:09:09 -0800

    feat(01-pizza-menu): create Pizza component

--- 01-pizza-menu/src/App.jsx
+++ 01-pizza-menu/src/App.jsx
@@ -1,5 +1,22 @@
 function App() {
-  return <h1>Hello React + vite!</h1>;
+  return (
+    <div>
+      <h1>Hello React + vite!</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="../public/pizzas/spinaci.jpg" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
 }
 
 export default App;
