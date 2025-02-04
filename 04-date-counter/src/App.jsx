2767926ab02466b0b6051c671fdcf65dca30a411
LiliGhaznavi <ghaznavizahra77@gmail.com>
2025-02-04 06:58:09 -0800

    feat(04-date-counter): Add Counter component

--- 04-date-counter/src/App.jsx
+++ 04-date-counter/src/App.jsx
@@ -1,3 +1,26 @@
 export default function App() {
-  return <h1>Hello React + Vite!</h1>;
+  return <Counter />;
+}
+
+function Counter() {
+  return (
+    <>
+      <button>&times;</button>
+      {
+        <main className="container">
+          <div>
+            <button>-</button>
+            <h2>Step: 1</h2>
+            <button>+</button>
+          </div>
+          <div>
+            <button>-</button>
+            <h2>Count: 1</h2>
+            <button>+</button>
+          </div>
+          <p>Today is Mon, Feb 4 2025.</p>
+        </main>
+      }
+    </>
+  );
 }
