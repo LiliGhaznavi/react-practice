c9920565e3490c070b216c6d2c6a0d50a93e08a6
LiliGhaznavi <ghaznavizahra77@gmail.com>
2025-02-05 03:47:00 -0800

    feat(05-travel-list): Add Item component

--- 05-travel-list/src/App.jsx
+++ 05-travel-list/src/App.jsx
@@ -1,5 +1,10 @@
 import "./index.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -24,7 +29,26 @@ const Form = () => {
 };
 
 const PackingList = () => {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 };
 
 const Stats = () => {
