fedd1341eb67146ad90a595039901dc00f10104d
LiliGhaznavi <ghaznavizahra77@gmail.com>
2025-02-05 01:02:19 -0800

    feat(05-travel-list): Add static code and styles

---
+++ 05-travel-list/src/App.jsx
@@ -0,0 +1,36 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+const Logo = () => {
+  return <h1>🏝️ Far Away 🧳</h1>;
+};
+
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+};
+
+const PackingList = () => {
+  return <div className="list">LIST</div>;
+};
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%).</em>
+    </footer>
+  );
+};
