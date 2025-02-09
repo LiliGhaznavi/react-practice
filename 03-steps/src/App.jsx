a1a655b2eebff41eb87c6dd4ccc43037fb1262cb
LiliGhaznavi <ghaznavizahra77@gmail.com>
2025-02-09 01:09:35 -0800

    refactor(03-steps): Replace paragraph with StepMessage component using children prop

--- 03-steps/src/App.jsx
+++ 03-steps/src/App.jsx
@@ -35,9 +35,7 @@ function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
             <Button bgColor="#7950f2" color="#fff" onClick={handlePrevious}>
               <span>👈</span> Previous
@@ -49,6 +47,15 @@ function App() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <p className="message">
+      <h3>Step {step}:</h3>
+      {children}
+    </p>
   );
 }
 
