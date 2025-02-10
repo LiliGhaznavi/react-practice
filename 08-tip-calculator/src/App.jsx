6e8bf5a1e71abeff1c50e381426a9e6089f3f2dc
LiliGhaznavi <ghaznavizahra77@gmail.com>
2025-02-10 01:31:03 -0800

    feat(08-tip-calculator): Add dynamic functionality

--- 08-tip-calculator/src/App.jsx
+++ 08-tip-calculator/src/App.jsx
@@ -1,42 +1,83 @@
+import { useState } from "react";
+
 export default function App() {
+  const [bill, setBill] = useState(0);
+  const [yourPercentage, setYourPercentage] = useState(0);
+  const [friendPercentage, setFriendPercentage] = useState(0);
+
+  function handleReset() {
+    setBill(0);
+    setYourPercentage(0);
+    setFriendPercentage(0);
+  }
+
   return (
     <div>
-      <BillInput />
-      <SelectPercentage id={1} />
-      <SelectPercentage id={2} />
-      <Output />
-      <Reset />
+      <BillInput bill={bill} OnAddBill={setBill} />
+      <SelectPercentage
+        id={1}
+        percentage={yourPercentage}
+        onAddPercentage={setYourPercentage}
+      >
+        How did you like the service?
+      </SelectPercentage>
+      <SelectPercentage
+        id={2}
+        percentage={friendPercentage}
+        onAddPercentage={setFriendPercentage}
+      >
+        How did your friend like the service?
+      </SelectPercentage>
+      <Output
+        bill={bill}
+        yourPercentage={yourPercentage}
+        friendPercentage={friendPercentage}
+      />
+      <Reset onReset={handleReset} />
     </div>
   );
 }
 
-function BillInput() {
+function BillInput({ bill, OnAddBill }) {
   return (
     <div>
       <label htmlFor="billAmount">How much was the bill?</label>
-      <input type="text" id="billAmount" />
+      <input
+        value={bill}
+        onChange={(e) => OnAddBill(Number(e.target.value))}
+        type="text"
+        id="billAmount"
+      />
     </div>
   );
 }
 
-function SelectPercentage({ id }) {
+function SelectPercentage({ id, percentage, onAddPercentage, children }) {
   return (
     <div>
-      <label htmlFor="serviceRating">How did you like the service?</label>
-      <select id={`serviceRating${id}`}>
-        <option>Dessatisfied(0%)</option>
-        <option>It was okay(5%)</option>
-        <option>It was good(10%)</option>
-        <option>Absolutely amazing!d(20%)</option>
+      <label htmlFor={`serviceRating${id}`}>{children}</label>
+      <select
+        value={percentage}
+        onChange={(e) => onAddPercentage(Number(e.target.value))}
+        id={`serviceRating${id}`}
+      >
+        <option value={0}>Dessatisfied(0%)</option>
+        <option value={5}>It was okay(5%)</option>
+        <option value={10}>It was good(10%)</option>
+        <option value={20}>Absolutely amazing!d(20%)</option>
       </select>
     </div>
   );
 }
 
-function Output() {
-  return <h1>You pay $100 ($100 + $5tip)</h1>;
+function Output({ bill, yourPercentage, friendPercentage }) {
+  const tipAverage = (yourPercentage + friendPercentage) / 2;
+
+  return (
+    <h1>{`You pay $${bill + tipAverage} ($${bill} + $${tipAverage} tip)`}</h1>
+  );
 }
 
-function Reset() {
-  return <button>Reset</button>;
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
 }
