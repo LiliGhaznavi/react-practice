3cf1956d3f16e88105a529b789c4942a2be2e075
LiliGhaznavi <ghaznavizahra77@gmail.com>
2025-02-17 13:40:13 -0800

    feat(10-guess-my-number): Add static code

--- 10-guess-my-number/src/App.jsx
+++ 10-guess-my-number/src/App.jsx
@@ -1,5 +1,41 @@
 import "./index.css";
 
 export default function App() {
-  return <h1>Hello React + Vite!</h1>;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Guess My Number!</h1>
+      <p className="between">(Between 1 and 20)</p>
+      <button className="btn again">Again!</button>
+      <div className="number">?</div>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <section className="left">
+        <input type="number" className="guess" />
+        <button className="btn check">Check!</button>
+      </section>
+      <section className="right">
+        <p className="message">Start guessing...</p>
+        <p className="label-score">
+          💯 Score: {<sapn className="score">20</sapn>}
+        </p>
+        <p className="label-highscore">
+          🥇 Highscore: <span className="highscore">0</span>
+        </p>
+      </section>
+    </main>
+  );
 }
